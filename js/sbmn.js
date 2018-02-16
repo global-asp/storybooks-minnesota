6cac3713ae4a8614f6713a993f86968e2ef34ade
dohliam <dohliam@gmail.com>
2018-02-15 16:54:23 -0800

    update site

--- js/sbmn.js
+++ js/sbmn.js
@@ -19,28 +19,28 @@ function playpause(n) {
 
 function switchlang(d,l) {
   en = document.getElementsByClassName("en");
-  sw = document.getElementsByClassName("sw");
+//   sw = document.getElementsByClassName("sw");
   def = document.getElementsByClassName("def");
   btn_en = document.getElementsByClassName("lang-en");
-  btn_sw = document.getElementsByClassName("lang-sw");
+//   btn_sw = document.getElementsByClassName("lang-sw");
   if (l == "en") {
     for (var i = 0; i < btn_en.length + 1; i++) {
-      sw[i].style.display = "none";
+//       sw[i].style.display = "none";
       en[i].style.display = "block";
       def[i].style.display = "none";
       btn_en[i].innerHTML = d;
-      btn_sw[i].innerHTML = "sw";
+//       btn_sw[i].innerHTML = "sw";
       btn_en[i].setAttribute("onclick", "switchlang('" + d + "','" + d + "')");
-      btn_sw[i].setAttribute("onclick", "switchlang('" + d + "','sw')");
+//       btn_sw[i].setAttribute("onclick", "switchlang('" + d + "','sw')");
     }
   } else if (l == "sw") {
     for (var i = 0; i < btn_sw.length + 1; i++) {
       en[i].style.display = "none";
-      sw[i].style.display = "block";
+//       sw[i].style.display = "block";
       def[i].style.display = "none";
       btn_en[i].innerHTML = "en";
-      btn_sw[i].innerHTML = d;
-      btn_sw[i].setAttribute("onclick", "switchlang('" + d + "','" + d + "')");
+//       btn_sw[i].innerHTML = d;
+//       btn_sw[i].setAttribute("onclick", "switchlang('" + d + "','" + d + "')");
       btn_en[i].setAttribute("onclick", "switchlang('" + d + "','en')");
     }
   } else if (l == d) {
@@ -49,9 +49,9 @@ function switchlang(d,l) {
       sw[i].style.display = "none";
       def[i].style.display = "block";
       btn_en[i].innerHTML = "en";
-      btn_sw[i].innerHTML = "sw";
+//       btn_sw[i].innerHTML = "sw";
       btn_en[i].setAttribute("onclick", "switchlang('" + d + "','en')");
-      btn_sw[i].setAttribute("onclick", "switchlang('" + d + "','sw')");
+//       btn_sw[i].setAttribute("onclick", "switchlang('" + d + "','sw')");
     }
   }
 }
